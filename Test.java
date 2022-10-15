2c0a66c367fbd749ac80cd173632dadfa803fcfa
kishore124 <manikantakishore124@gmail.com>
2022-10-15 19:57:19 +0530

    First commit

---
+++ Test.java
@@ -0,0 +1,15 @@
+public class Test{
+    public static void main(String []args){
+        System.out.println("Hello World");
+        First obj =new First();
+        obj.Hello();
+    }
+}
+ class First{
+   public void Hello(){
+    System.out.println("In Fist class");
+   }
+}
+class Second{
+
+}
